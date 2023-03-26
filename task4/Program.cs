df3a5f148176233cf50bb26f0f8230447011dbf6
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-03-26 11:27:38 +0300

    Task4 added

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.tion
+
+System.Console.Write("введите число N:");
+
+int N=int.Parse(Console.ReadLine());
+
+for(int i=2;i<=N;i=i+2)
+{
+    Console.WriteLine(i);
+}
